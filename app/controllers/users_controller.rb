60f984b44c104e3dd63b79ffbb48fde28002a4e0
sakitakayanagi <melomelo.mymelody.sanrio@gmail.com>
2022-05-24 21:50:39 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,8 @@
 class UsersController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-  
-  
-  def index
-  
-  end
-  
-  def new
-    @user = User.new 
-  end
-
-  def edit
-  end
 
   def show
     @user = User.find(params[:id])
-    @prototypes = Prototype.all
+    @prototypes = @user.prototypes
   end
-
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :profile, :occupation, :position).merge(user_id: current_user.id)
-    # .merge(user_id: current_user.id)
-  end  
 
 end
